37c1a9d21d0fd1819761e5168b7f6b21f7dd07a0
George Nikishin <mail2giz@gmail.com>
2021-03-16 21:52:49 +0300

    update

--- install.sh
+++ install.sh
@@ -1,50 +1,16 @@
 #!/usr/env bash
 
 #Update
-sudo pacman -Suy
+sudo apt update
 
-#install software from default repos
-sudo pacman -S \
-    xorg \
-    xorg-server \
-    xorg-init \
-    xf86-video-nouveau \
-    bspwm \
-    sxhkd \
-    alacritty \
-    neovim \
-    tmux \
-    htop \
-    newsboat \
-    git \
-    ranger \
-    feh \
-    pulseaudio \
-    pulsemixer \
-    picom \
-    shellcheck \
-    ctags \
-    python-pip \
-    awesome-terminal-fonts \
-    go \
-    curl #just in case
-
-#Install AUR helper
-mkdir "${HOME}"/tmp && \
-    cd "${HOME}"/tmp && \
-    git clone https://aur.archlinux.org/yay.git . && \
-    makepkg -si && \
-    cd "${HOME}" && \
-    rm -r "${HOME}"/tmp
-
-#Install AUR software
-yay -S polybar ttf-nerd-fonts-hack-complete-git
+#Install packages
+sudo apt install vim tmux tig
 
 #Install configs
 git clone https://github.com/blackmou5e/dotfiles.git
-ln -s "${HOME}"/dotfiles/.Xresources "${HOME}"/.Xresources
-ln -s "${HOME}"/dotfiles/config/* "${HOME}"/.config/
+ln -s "${HOME}"/dotfiles/config/vim "${HOME}"/.vimrc
 
-nvim && ./"${HOME}"/.config/nvim/plugged/YouCompleteMe/install.py --clang-completion --go-completion
+#Install vim plugins
+vim +PlugInstall +qall
 
 exit 0
