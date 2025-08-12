34b4ccd009f313085b861d49f4206f8b7c3aedf0
George Nikishin <mail2giz@gmail.com>
2025-08-12 20:11:19 +0300

    added oil config

--- nvim_nightly/init.lua
+++ nvim_nightly/init.lua
@@ -117,7 +117,24 @@ vim.cmd("colorscheme vague")
 
 
 -- plugins init
-require("oil").setup()
+require("oil").setup({
+    view_options = {
+        show_hidden = true,
+        is_hidden_file = function(name, bufnr)
+            local m = name:match("^%.")
+            return m ~= nil
+        end,
+        is_always_hidden = function(name, bufnr)
+            return false
+        end,
+        natural_order = "fast",
+        case_insensitive = false,
+        sort = {
+            { "type", "asc" },
+            { "name", "asc" },
+        },
+    }
+})
 require("mini.pick").setup()
 
 -- some keymaps
