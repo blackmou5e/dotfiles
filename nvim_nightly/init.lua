9252d1ef79531399071ab1ae6d5196e892757e03
George Nikishin <mail2giz@gmail.com>
2025-08-14 01:53:06 +0300

    changed theme, a bit of formatting

--- nvim_nightly/init.lua
+++ nvim_nightly/init.lua
@@ -15,35 +15,34 @@ vim.o.hlsearch = false
 vim.o.incsearch = true
 vim.o.scrolloff = 8
 
-
 -- leader, obviously
 vim.g.mapleader = " "
 
 -- New built-in plugin manager, here we go guys!
 vim.pack.add({
-    { src = "https://github.com/vague2k/vague.nvim" },
-    { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/echasnovski/mini.pick" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-    { src = "https://github.com/L3MON4D3/LuaSnip" },
-    { src = "https://github.com/chomosuke/typst-preview.nvim" },
-    { src = "https://github.com/p00f/clangd_extensions.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+	{ src = "https://github.com/p00f/clangd_extensions.nvim" },
+	{ src = "https://github.com/tahayvr/matteblack.nvim" },
 })
 
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "lua", "luadoc", "c", "cpp", "terraform", "helm", "go" },
-    auto_install = false,
-    highlight = { enabled = true },
+	ensure_installed = { "lua", "luadoc", "c", "cpp", "terraform", "helm", "go" },
+	auto_install = false,
+	highlight = { enabled = true },
 })
 
 -- A bit of omnicomplete and lsp magic, to enable them doing love
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-        end
-    end,
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method("textDocument/completion") then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
 })
 vim.cmd("set completeopt+=noselect")
 
@@ -51,23 +50,23 @@ vim.lsp.enable({ "lua_ls", "clangd", "gopls", "helm_ls", "tinymist" })
 
 -- diagnostics setup
 vim.diagnostic.config({
-    update_in_insert = true,
-    virtual_text = false,
-    underline = true,
-    virtual_lines = true,
-    signs = true,
-    float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "if_many",
-        header = "",
-        prefix = "",
-    }
+	update_in_insert = true,
+	virtual_text = false,
+	underline = true,
+	virtual_lines = true,
+	signs = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "if_many",
+		header = "",
+		prefix = "",
+	},
 })
 
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 
 -- snippets
 local ls = require("luasnip")
@@ -78,78 +77,30 @@ vim.keymap.set({ "i" }, "<C-e>", function() ls.expand() end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
 
-
--- Theme settings, nothing interesting in particular
-require("vague").setup({
-    transparent = true, -- don't set background
-    -- disable bold/italic globally in `style`
-    bold = true,
-    italic = true,
-    style = {
-        -- "none" is the same thing as default. But "italic" and "bold" are also valid oions
-        boolean = "bold",
-        number = "none",
-        float = "none",
-        error = "bold",
-        comments = "italic",
-        conditionals = "none",
-        functions = "none",
-        headings = "bold",
-        operators = "none",
-        strings = "none",
-        variables = "none",
-
-        -- keywords
-        keywords = "none",
-        keyword_return = "italic",
-        keywords_loop = "none",
-        keywords_label = "none",
-        keywords_exception = "none",
-
-        -- builtin
-        builtin_constants = "bold",
-        builtin_functions = "none",
-        builtin_types = "bold",
-        builtin_variables = "none",
-    },
-    -- plugin styles where applicable
-    -- make an issue/pr if you'd like to see more styling oions!
-    plugins = {
-        lsp = {
-            diagnostic_error = "bold",
-            diagnostic_hint = "none",
-            diagnostic_info = "italic",
-            diagnostic_ok = "none",
-            diagnostic_warn = "bold",
-        },
-    },
-})
-
-vim.cmd("colorscheme vague")
-
+vim.cmd("colorscheme matteblack")
 
 -- plugins init
 require("oil").setup({
-    view_options = {
-        show_hidden = true,
-        is_hidden_file = function(name, bufnr)
-            local m = name:match("^%.")
-            return m ~= nil
-        end,
-        is_always_hidden = function(name, bufnr)
-            return false
-        end,
-        natural_order = "fast",
-        case_insensitive = false,
-        sort = {
-            { "type", "asc" },
-            { "name", "asc" },
-        },
-    }
+	view_options = {
+		show_hidden = true,
+		is_hidden_file = function(name, bufnr)
+			local m = name:match("^%.")
+			return m ~= nil
+		end,
+		is_always_hidden = function(name, bufnr)
+			return false
+		end,
+		natural_order = "fast",
+		case_insensitive = false,
+		sort = {
+			{ "type", "asc" },
+			{ "name", "asc" },
+		},
+	},
 })
 require("mini.pick").setup()
 
 -- some keymaps
-vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
-vim.keymap.set('n', '<leader>e', ':Oil<CR>')
+vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
+vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
