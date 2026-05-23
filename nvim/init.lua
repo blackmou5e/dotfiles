7fabefb3a70f1d18acd8b37ee0abfdd697e67f24
George Nikishin <mail2giz@gmail.com>
2026-05-23 10:31:51 +0300

    added fff plugin, removed dangling symlink

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,7 +34,27 @@ vim.pack.add({
     { src = "https://github.com/ravsii/tree-sitter-d2" },
     { src = "https://github.com/benomahony/uv.nvim" },
     { src = "folke/tokyonight.nvim" },
+    { src = "https://github.com/dmtrKovalenko/fff.nvim" },
 })
+
+
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if name == 'fff.nvim' and (kind == 'install' or kind == 'update') then
+      if not ev.data.active then vim.cmd.packadd('fff.nvim') end
+      require('fff.download').download_or_build_binary()
+    end
+  end,
+})
+
+vim.g.fff = {
+  lazy_sync = true,
+  debug = { enabled = true, show_scores = true },
+}
+
+vim.keymap.set('n', 'ff', function() require('fff').find_files() end, { desc = 'FFFind files' })
+
 
 
 require("nvim-treesitter").setup({
